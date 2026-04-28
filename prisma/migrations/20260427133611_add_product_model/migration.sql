7dacace3c3ae330f59ffcaa827363bd30d33b0f1
semihalperKeskin <semihalperkeskin@gmail.com>
2026-04-28 14:49:32 +0300

    feat: add Product model and migration script

---
+++ prisma/migrations/20260427133611_add_product_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "imageUrl" TEXT,
+    "category" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
